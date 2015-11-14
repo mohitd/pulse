d7e96888eb1240d6ce0fbbf3df040234a8e3971c
Mohit Deshpande <mohitd2000@gmail.com>
2015-11-14 18:44:44 -0500

    Added support for location

--- Pulse/mobile/src/main/java/com/isjctu/pulse/MainActivity.java
+++ Pulse/mobile/src/main/java/com/isjctu/pulse/MainActivity.java
@@ -1,16 +1,60 @@
 package com.isjctu.pulse;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    GoogleApiClient apiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        apiClient = new GoogleApiClient.Builder(this, this, this).addApi(LocationServices.API).build();
+        apiClient.connect();
+
         Intent myIntent = new Intent(this, SyncService.class);
         this.startService(myIntent);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        LocationServices.FusedLocationApi.requestLocationUpdates(apiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        apiClient.disconnect();
+        LocationServices.FusedLocationApi.removeLocationUpdates(apiClient, this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
     }
 }
